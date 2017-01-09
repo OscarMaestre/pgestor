fe5a58d34a845e8b8150c6300b44a0ce1188b567
Oscar G.G <profesor.oscar.gomez@gmail.com>
2017-01-09 22:35:18 +0100

    Añadidos campos

--- gestor/bd/models.py
+++ gestor/bd/models.py
@@ -1,18 +1,68 @@
 from django.db import models
-
+from django.dispatch import receiver
+from django.db.models.signals import pre_save, post_init
 # Create your models here.
 
+def transponer(cadena, clave, sumar=True):
+    pos_clave=0
+    cad_devuelta=""
+    longitud_clave = len(clave)
+    numero_cadena=[]
+    numeros_despues=[]
+    numero_clave=[]
+    for i in range(0, len(cadena)):
+        simbolo=cadena[i]
+        o_simbolo=ord(cadena[i])
+        numero_cadena.append ( o_simbolo )
+        despl=ord(clave[pos_clave])
+        numero_clave.append(despl)
+        if sumar:
+            nuevo_simbolo=(o_simbolo + despl ) % 255
+            numeros_despues.append(nuevo_simbolo)
+            
+        else :
+            nuevo_simbolo=(o_simbolo - despl ) % 255
+            numeros_despues.append(nuevo_simbolo)
+            
+            
+        cad_devuelta += chr(nuevo_simbolo)
+        pos_clave = ( pos_clave + 1 ) % longitud_clave
+    return cad_devuelta
+
+
 def cifrar(cadena, clave):
-    return cadena
+    
+    return transponer ( cadena, clave, True)
 
 def descifrar(cadena, clave):
-    return cadena
+    return transponer ( cadena, clave, False)
 
+
+    
 class Grupo(models.Model):
-    nombre  =   models.CharField(max_length=8)
+    nombre  =   models.CharField(max_length=8)    
     
     def __str__(self):
         return self.nombre
+
+@receiver(pre_save, sender=Grupo)
+def almacenar_grupo(sender, **kwargs):
+    instancia=kwargs["instance"]
+    nombre=instancia.nombre
+    nombre_cifrado=cifrar(nombre, "1234")
+    print (nombre, nombre_cifrado)
+    instancia.nombre=nombre_cifrado
+    kwargs["instance"]=instancia
+
+@receiver(post_init, sender=Grupo)
+def cargar_grupo(sender, **kwargs):
+    instancia=kwargs["instance"]
+    nombre=instancia.nombre
+    nombre_cifrado=descifrar(nombre, "1234")
+    #print (nombre, nombre_cifrado)
+    instancia.nombre=nombre_cifrado
+    kwargs["instance"]=instancia    
+
     
 class Modulo(models.Model):
     nombre          =   models.CharField(max_length=120)
@@ -31,3 +81,5 @@ class Alumno(models.Model):
     num_exp             =   models.CharField(max_length=10)
     foto                =   models.ImageField(upload_to="fotos")
     modulos             =   models.ManyToManyField(Modulo)
+    modular             =   models.BooleanField()
+    inmigrante          =   models.BooleanField()
--- gestor/bd/tests.py
+++ gestor/bd/tests.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 from django.test import TestCase
 
 # Create your tests here.
@@ -6,7 +7,36 @@ from .models import *
 
 class CifradoTest(TestCase):
     def test_cifrado_simetrico(self):
-        cadena="1234"
+        cadena="áéíóúAEIOU"
         cadena_cifrada=cifrar(cadena, "abc")
+        #print(cadena_cifrada)
         cadena_descifrada=descifrar(cadena_cifrada, "abc")
-        self.assertEqual(cadena_cifrada, cadena_descifrada)
+        #print(cadena_descifrada)
+        self.assertEqual(cadena, cadena_descifrada)
+        
+    def test_cifrado_2(self):
+        cadena="áéíóúAEIOU "
+        clave="ÑñÚü"
+        cadena_cifrada=cifrar(cadena, clave)
+        #print(cadena_cifrada)
+        cadena_descifrada=descifrar(cadena_cifrada, clave)
+        #print(cadena_descifrada)
+        self.assertEqual(cadena, cadena_descifrada) 
+        
+    def test_cifrado_3(self):
+        cadena="!#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ÝÞßàáâãäåæçèéêëìíîïðñòó"
+        clave="ÑñÚü"
+        cadena_cifrada=cifrar(cadena, clave)
+        #print(cadena_cifrada)
+        cadena_descifrada=descifrar(cadena_cifrada, clave)
+        #print(cadena_descifrada)
+        self.assertEqual(cadena, cadena_descifrada)
+        
+    def test_cifrado_4(self):
+        cadena="!#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ÝÞßàáâãäåæçèéêëìíîïðñòó"
+        clave=cadena[4:40]
+        cadena_cifrada=cifrar(cadena, clave)
+        #print(cadena_cifrada)
+        cadena_descifrada=descifrar(cadena_cifrada, clave)
+        #print(cadena_descifrada)
+        self.assertEqual(cadena, cadena_descifrada)
